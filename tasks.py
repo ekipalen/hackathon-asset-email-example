89838d3fe7e13ae439b7df68a6ea7ad3f7967a2b
Erik Palen <84192057+ekipalen@users.noreply.github.com>
2023-10-05 16:00:10 +0300

    Update tasks.py
    Added testing options

--- tasks.py
+++ tasks.py
@@ -8,6 +8,9 @@ import io
 LEAD_ASSET = 'Hackathon Leads'
 TEMPLATE_ASSET = 'Hackathon html template'
 EMAIL_SUBJECT = 'Example email from the Control Room'
+TESTING_MODE = 'Yes'  # Set to "No" to send emails for the actual recipients.
+TEST_RECIPIENT = 'eki.palen@gmail.com'
+SENDER = 'You <you@example.com>'
 
 @task
 def minimal_task():
@@ -24,15 +27,18 @@ def minimal_task():
         email_body = email_body.replace('[COMPANY]',lead['company'])
         email_body = email_body.replace('[COMPANYUPPER]',lead['company'].upper())
         print(email_body)
+        if TESTING_MODE == 'No':
+            send_email(email_body,lead['email'])
     
-    send_email(email_body)
+    if TESTING_MODE == 'Yes':
+        send_email(email_body,TEST_RECIPIENT)
     
-def send_email(email_body):
+def send_email(email_body,recipient):
     mailgun_secret = vault.get_secret('Mailgun')
     return requests.post(
         "https://api.mailgun.net/v3/"+mailgun_secret['domain']+"/messages",
         auth=("api", mailgun_secret['api_key']),
-        data={"from": mailgun_secret['sender'],
-              "to": mailgun_secret['recipient'],
+        data={"from": SENDER,
+              "to": recipient,
               "subject": EMAIL_SUBJECT,
               "html": email_body})
